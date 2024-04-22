dac3acadac7ba9435247dc2416acea1692c10d5a
victor_marquez <victorsheepman@gmail.com>
2024-04-22 12:35:05 -0400

    progress

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
-import { useContext} from "react";
+import { useContext, useEffect, useMemo, useState} from "react";
 import { CoffeeContext } from "../context";
 
 export const Home = () => {
   const {coffeeList} = useContext(CoffeeContext);
+  const [isNow, setIsNow] = useState<Boolean>(true)
+
+
+  const list = useMemo(() => {
+    if (isNow) {
+      return coffeeList.filter(coffe=>coffe.available === true)
+    }else{
+      return coffeeList
+    }
+  }, [isNow])
+
+  useEffect(() => {
+    console.log(list)
+  }, [isNow])
+  
+
   return (
     <div className="home">
      
@@ -14,6 +30,14 @@ export const Home = () => {
             <p className="fontBody">
               Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.
             </p>
+        </div>
+        <div className="home_container_filter">
+          <button className={isNow === false ? "home_container_filter_active": "home_container_filter_desactive"} onClick={()=>setIsNow(false)}>
+            All Products
+          </button>
+          <button className={isNow === true ? "home_container_filter_active": "home_container_filter_desactive"} onClick={()=>setIsNow(true)}>
+            Available Now
+          </button>
         </div>
       </div>
     </div>
