186de872ab79ba48c38fd977c5baf5584c28c336
victor_marquez <victorsheepman@gmail.com>
2024-04-22 11:37:46 -0400

    progress

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,17 @@
 export const Home = () => {
   return (
     <div className="home">
-      <figure className="home_figure">
-        <img src="bg-cafe.jpg" alt="" />
-      </figure>
-  
+     
+      <div className="home_container">
+        <div className="home_container_header">
+            <h1 className="fontHeading">
+              Our Collection
+            </h1>
+            <p className="fontBody">
+              Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.
+            </p>
+        </div>
+      </div>
     </div>
   )
 }
